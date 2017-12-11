5acc99d5697e61917ef75fe97fb6e5824ace65c9
kulingkwi <kulingkwi@gmail.com>
2017-12-11 13:29:47 +0800

    Finish master election in synchronized way

--- zk/src/main/java/org/kulingkwi/learning/zk/Master.java
+++ zk/src/main/java/org/kulingkwi/learning/zk/Master.java
@@ -1,13 +1,17 @@
 package org.kulingkwi.learning.zk;
 
-import org.apache.zookeeper.WatchedEvent;
-import org.apache.zookeeper.ZooKeeper;
-import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.*;
+import org.apache.zookeeper.data.Stat;
+
+import java.util.Random;
 
 public class Master implements Watcher {
 
     ZooKeeper zk;
     String hostPort;
+
+    String serverId = Long.toHexString(new Random().nextLong());
+    boolean isLeader = false;
 
     Master(String hostPort) {
         this.hostPort = hostPort;
@@ -15,6 +19,45 @@ public class Master implements Watcher {
 
     void startZk() throws Exception {
         zk = new ZooKeeper(hostPort, 15000, this);
+    }
+
+    void stopZk() throws Exception {
+        zk.close();
+    }
+
+    /** return true if there is a master */
+    boolean checkMaster() {
+        while(true) {
+            try {
+                Stat stat = new Stat();
+                byte[] data = zk.getData("/master", false, stat);
+                isLeader = new String(data).equals(serverId);
+                return true;
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+                return false;
+            } catch (KeeperException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /** run for master election */
+    void runForMaster() throws InterruptedException {
+        while(true) {
+            try{
+                zk.create("/master", serverId.getBytes(), ZooDefs.Ids.OPEN_ACL_UNSAFE, CreateMode.EPHEMERAL);
+                isLeader = true;
+            } catch (KeeperException.NodeExistsException e) {
+                isLeader = false;
+                break;
+            } catch (KeeperException.ConnectionLossException e) {
+                System.out.println(e);
+            } catch (Exception e) {
+                System.out.println(e);
+            }
+            if (checkMaster()) break;
+        }
     }
 
     public void process(WatchedEvent watchedEvent) {
@@ -25,8 +68,16 @@ public class Master implements Watcher {
         String zkCon = args != null && args.length > 0 && args[0] != null ? args[0]:"127.0.0.1:2181";
         Master master = new Master(zkCon);
         master.startZk();
+        master.runForMaster();
 
-        //wait for a bit
-        Thread.sleep(60000);
+        if (master.isLeader) {
+            System.out.println("I am the leader");
+            //wait for a bit
+            Thread.sleep(60000);
+        } else {
+            System.out.println("Someone else is the leader");
+        }
+
+        master.stopZk();
     }
 }
